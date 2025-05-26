2393281110401e7d53bd1875b279c91c3fe188a4
gatsby100m <gritoujoshua66@gmail.com>
2025-05-26 20:23:37 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,19 @@
+import { useEffect, useState } from 'react'
+import { supabase } from './utils/supabase'
+import LiveMatches from './components/LiveMatches'
 
+export default function App() {
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setUser(session?.user || null)
+    })
+  }, [])
+
+  return (
+    <div className="app">
+      <LiveMatches />
+    </div>
+  )
+}
