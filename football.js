01673e4a4d17a8972c955943b0a623328c97014e
gatsby100m <gritoujoshua66@gmail.com>
2025-05-26 23:02:26 +0100

    Delete football.js

--- football.js
+++
@@ -1,6 +0,0 @@
-export const fetchLiveMatches = async () => {
-  const res = await fetch('https://v3.football.api-sports.io/fixtures?live=all', {
-    headers: { 'x-apisports-key': '407ac7fdde57217877e224cf7ed3d66d' }
-  })
-  return (await res.json()).response || []
-}
